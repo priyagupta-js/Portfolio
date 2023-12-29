9fd52bba779739a0238a722f5fb36b921fa374e7
Priya Gupta <priyagupta052k@gmail.com>
2023-12-29 15:34:07 +0530

    Add Auto-typed feature

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // console.log("Hello World");
+
+let typed = new Typed(".auto-type",{
+    strings :["Frontend Development" , "Graphic Design"],
+    typeSpeed : 45,
+    backSpeed : 45,
+    loop : true
+})
