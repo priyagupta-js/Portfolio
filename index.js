d407006ae4aed58cccc1ba2fbfbbdc74c4e3572f
Priya Gupta <priyagupta052k@gmail.com>
2025-10-31 14:21:40 +0530

    Update UI

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // console.log("Hello World");
 
 let typed = new Typed(".auto-type",{
-    strings :["Frontend Development" , "Graphic Design"],
+    strings :["Frontend Development" , "FullStack Web Development"],
     typeSpeed : 45,
     backSpeed : 45,
     loop : true
